8e7a8104ff8f1ee3a8b8124c1275bc96525a82cb
programmer123pro <taras.ivankiv.22@pnu.edu.ua>
2024-11-11 22:13:51 +0200

    lab5

--- app/users/views.py
+++ app/users/views.py
@@ -18,4 +18,78 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
+
+users = {
+    'taras' : "123"
+}
+
+@users_bp.route("/login", methods=['GET', 'POST'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+ 
+        if(username and password):
+            if(username in users and users[username] == password):
+                session['username'] = username
+                flash('Login successfull', 'success')
+                return redirect(url_for('users.profile'))
+            else:   
+               flash('Login failed. Please try again.', 'error') 
     
+    return render_template("login.html")
+
+@users_bp.route("/profile")
+def profile():
+    if('username' in session):
+        cookies = list(request.cookies.items())
+        dark_theme = False 
+        if ('theme' in request.cookies) and (request.cookies['theme'] == 'dark'):
+            dark_theme = True
+        return render_template("profile.html", username=session['username'], cookies=cookies, dark_theme=dark_theme)
+    else:
+        return redirect(url_for('users.login'))
+
+@users_bp.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('users.login')) 
+
+@users_bp.route('/cookies', methods=['POST'])
+def cookies():
+    response = redirect(url_for('users.profile'))
+    method = request.form.get('_method')
+
+    if method == 'POST':
+        cookieKey = request.form.get('cookieKey')
+        cookieValue = request.form.get('cookieValue')
+        term = int(request.form.get('cookieExpires')) * 86400
+        response.set_cookie(cookieKey, cookieValue, term)
+        flash('Cookie успішно додано!', 'success')
+    
+    elif method == 'DELETE':
+        cookieKey = request.form.get('deleteKey')
+        if cookieKey in request.cookies:
+            response.delete_cookie(cookieKey)
+            flash('Cookie успішно видалено!', 'success')
+        else:
+            flash('Такого Cookie не знайдено!', 'error')
+
+    elif method == 'ALL_DELETE':
+            for cookie in request.cookies:
+                response.delete_cookie(cookie)
+            flash('Cookies успішно видалені!', 'success')
+            
+    return response
+
+@users_bp.route('/change_theme')
+def theme():
+    response = redirect(url_for('users.profile')) 
+    if not('theme' in request.cookies) or (request.cookies['theme'] == 'light'):
+        response.set_cookie('theme', 'dark')
+    elif(request.cookies['theme'] == 'dark'):
+        response.set_cookie('theme', 'light')
+    return response
+        
+
+            
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Launch built-in web server and run this Flask webapp, debug=True
-    
- 
+    app.run(debug=True, port=8080)  # Launch built-in web server and run this Flask webapp, debug=True
+
+
